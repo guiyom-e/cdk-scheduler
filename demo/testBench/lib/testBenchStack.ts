ef907734c16ef0ce4ec135567399c9a0de5e2c29
guiyom-e <guillaume.egee@outlook.com>
2022-07-01 17:07:19 +0200

    feat(testBench): add minimalist dashboard to monitor scheduler performance

--- demo/testBench/lib/testBenchStack.ts
+++ demo/testBench/lib/testBenchStack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import { Scheduler } from 'cdk-scheduler';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { aws_cloudwatch as cloudwatch } from 'aws-cdk-lib';
 
 export class TestBenchStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -47,5 +48,82 @@ export class TestBenchStack extends Stack {
 
     // ... and bind your consumer to this source
     receiveEventsHandler.addEventSource(eventSource);
+
+    const dashboard = new cloudwatch.Dashboard(this, 'TestBenchDashboard', {
+      dashboardName: 'TestBenchDashboard',
+      end: 'end',
+      periodOverride: cloudwatch.PeriodOverride.AUTO,
+      start: 'start',
+    });
+
+    const parsingRegex =
+      /.*Id:\s(?<testId>\d+).*UniqueId:\s(?<uniqueId>[a-zA-Z0-9#-_]+).*DiffMs:\s(?<diff>-?\d+).*Source:\s(?<source>\w+).*/;
+
+    dashboard.addWidgets(
+      new cloudwatch.LogQueryWidget({
+        title: 'Received scheduled messages - Logs',
+        width: 24,
+        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+        view: cloudwatch.LogQueryVisualizationType.TABLE,
+        queryLines: [
+          'fields @timestamp, @message',
+          'sort @timestamp desc',
+          'filter @message like "[FOR_DASHBOARD]"',
+          `parse @message ${parsingRegex}`,
+          'display testId, source, diff, @message',
+          'limit 200',
+        ],
+      }),
+    );
+
+    dashboard.addWidgets(
+      new cloudwatch.LogQueryWidget({
+        title: 'Difference between set and real publication timestamp (ms)',
+        width: 24,
+        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+        view: cloudwatch.LogQueryVisualizationType.LINE,
+        queryLines: [
+          'fields @timestamp, @message',
+          'sort @timestamp desc',
+          'filter @message like "[FOR_DASHBOARD]"',
+          `parse @message ${parsingRegex}`,
+          'stats avg(diff), min(diff), max(diff) by bin(1min)',
+        ],
+      }),
+    );
+
+    dashboard.addWidgets(
+      new cloudwatch.LogQueryWidget({
+        title:
+          'Difference between set and real publication timestamp (ms), depending on handling lambda function (ExtractHandler or NearFutureHandler)',
+        width: 24,
+        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+        view: cloudwatch.LogQueryVisualizationType.BAR,
+        queryLines: [
+          'fields @timestamp, @message',
+          'sort @timestamp desc',
+          'filter @message like "[FOR_DASHBOARD]"',
+          `parse @message ${parsingRegex}`,
+          'stats avg(diff), min(diff), max(diff) by source',
+        ],
+      }),
+    );
+
+    dashboard.addWidgets(
+      new cloudwatch.LogQueryWidget({
+        title: 'Multiple receptions',
+        width: 24,
+        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+        view: cloudwatch.LogQueryVisualizationType.BAR,
+        queryLines: [
+          'fields @timestamp, @message',
+          'sort @timestamp desc',
+          'filter @message like "[FOR_DASHBOARD]"',
+          `parse @message ${parsingRegex}`,
+          'stats count(*) as countReception by uniqueId',
+          'filter countReception > 1',
+        ],
+      }),
+    );
   }
 }
