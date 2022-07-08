316d0c719f911451e7878d8b0faefb4d3b88f064
guiyom-e <guillaume.egee@outlook.com>
2022-07-08 18:05:54 +0200

    improve test dashboard

--- demo/testBench/lib/testBenchStack.ts
+++ demo/testBench/lib/testBenchStack.ts
@@ -59,71 +59,162 @@ export class TestBenchStack extends Stack {
     const parsingRegex =
       /.*Id:\s(?<testId>\d+).*UniqueId:\s(?<uniqueId>[a-zA-Z0-9#-_]+).*DiffMs:\s(?<diff>-?\d+).*Source:\s(?<source>\w+).*/;
 
-    dashboard.addWidgets(
-      new cloudwatch.LogQueryWidget({
-        title: 'Received scheduled messages - Logs',
-        width: 24,
-        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
-        view: cloudwatch.LogQueryVisualizationType.TABLE,
-        queryLines: [
-          'fields @timestamp, @message',
-          'sort @timestamp desc',
-          'filter @message like "[FOR_DASHBOARD]"',
-          `parse @message ${parsingRegex}`,
-          'display testId, source, diff, @message',
-          'limit 200',
-        ],
-      }),
-    );
+    const logsWidget = new cloudwatch.LogQueryWidget({
+      title: 'Received scheduled messages - Logs',
+      width: 18,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.TABLE,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'display testId, source, diff, @message',
+        'limit 200',
+      ],
+    });
+    logsWidget.position(0, 0);
+
+    const eventsCount = new cloudwatch.LogQueryWidget({
+      title: 'Number of received scheduled messages per hour',
+      width: 6,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.BAR,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'stats count(*) by bin(1h)',
+      ],
+    });
+    logsWidget.position(18, 0);
+
+    const delays = new cloudwatch.LogQueryWidget({
+      title: 'Difference between set and real publication timestamp (ms)',
+      width: 18,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.LINE,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'stats avg(diff), min(diff), max(diff) by bin(1min)',
+      ],
+    });
+    delays.position(0, 6);
+
+    const averageDelay = new cloudwatch.LogQueryWidget({
+      title:
+        'Average difference between set and real publication timestamp (ms)',
+      width: 6,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.BAR,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'stats avg(diff) by bin(1h)',
+      ],
+    });
+    averageDelay.position(18, 6);
+
+    const delaysExtractHandler = new cloudwatch.LogQueryWidget({
+      title:
+        'Difference between set and real publication timestamp (ms) [ExtractHandler]',
+      width: 12,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.LINE,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'filter source = "extract"',
+        'stats avg(diff), min(diff), max(diff) by bin(1min)',
+      ],
+    });
+    delaysExtractHandler.position(0, 12);
+
+    const delaysNearFutureHandler = new cloudwatch.LogQueryWidget({
+      title:
+        'Difference between set and real publication timestamp (ms) [NearFutureHandler]',
+      width: 12,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.LINE,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'filter source = "handleNearFuture"',
+        'stats avg(diff), min(diff), max(diff) by bin(1min)',
+      ],
+    });
+    delaysNearFutureHandler.position(12, 12);
+
+    const multipleReceptions = new cloudwatch.LogQueryWidget({
+      title: 'Multiple receptions',
+      width: 24,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.BAR,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'stats count(*) as countReception by uniqueId',
+        'filter countReception > 1',
+      ],
+    });
+    multipleReceptions.position(0, 18);
+
+    const multipleReceptionsExtractHandler = new cloudwatch.LogQueryWidget({
+      title: 'Multiple receptions (ExtractHandler)',
+      width: 12,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.BAR,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'filter source = "extract"',
+        'stats count(*) as countReception by uniqueId',
+        'filter countReception > 1',
+      ],
+    });
+    multipleReceptionsExtractHandler.position(0, 24);
+
+    const multipleReceptionsNearFutureHandler = new cloudwatch.LogQueryWidget({
+      title: 'Multiple receptions (NearFutureHandler)',
+      width: 12,
+      logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
+      view: cloudwatch.LogQueryVisualizationType.BAR,
+      queryLines: [
+        'fields @timestamp, @message',
+        'sort @timestamp desc',
+        'filter @message like "[FOR_DASHBOARD]"',
+        `parse @message ${parsingRegex}`,
+        'filter source = "handleNearFuture"',
+        'stats count(*) as countReception by uniqueId',
+        'filter countReception > 1',
+      ],
+    });
+    multipleReceptionsNearFutureHandler.position(12, 24);
 
     dashboard.addWidgets(
-      new cloudwatch.LogQueryWidget({
-        title: 'Difference between set and real publication timestamp (ms)',
-        width: 24,
-        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
-        view: cloudwatch.LogQueryVisualizationType.LINE,
-        queryLines: [
-          'fields @timestamp, @message',
-          'sort @timestamp desc',
-          'filter @message like "[FOR_DASHBOARD]"',
-          `parse @message ${parsingRegex}`,
-          'stats avg(diff), min(diff), max(diff) by bin(1min)',
-        ],
-      }),
-    );
-
-    dashboard.addWidgets(
-      new cloudwatch.LogQueryWidget({
-        title:
-          'Difference between set and real publication timestamp (ms), depending on handling lambda function (ExtractHandler or NearFutureHandler)',
-        width: 24,
-        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
-        view: cloudwatch.LogQueryVisualizationType.BAR,
-        queryLines: [
-          'fields @timestamp, @message',
-          'sort @timestamp desc',
-          'filter @message like "[FOR_DASHBOARD]"',
-          `parse @message ${parsingRegex}`,
-          'stats avg(diff), min(diff), max(diff) by source',
-        ],
-      }),
-    );
-
-    dashboard.addWidgets(
-      new cloudwatch.LogQueryWidget({
-        title: 'Multiple receptions',
-        width: 24,
-        logGroupNames: [receiveEventsHandler.logGroup.logGroupName],
-        view: cloudwatch.LogQueryVisualizationType.BAR,
-        queryLines: [
-          'fields @timestamp, @message',
-          'sort @timestamp desc',
-          'filter @message like "[FOR_DASHBOARD]"',
-          `parse @message ${parsingRegex}`,
-          'stats count(*) as countReception by uniqueId',
-          'filter countReception > 1',
-        ],
-      }),
+      logsWidget,
+      eventsCount,
+      delays,
+      averageDelay,
+      delaysExtractHandler,
+      delaysNearFutureHandler,
+      multipleReceptions,
+      multipleReceptionsExtractHandler,
+      multipleReceptionsNearFutureHandler,
     );
   }
 }
